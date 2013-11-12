81121d18286fb0fea9be6c25c3c2d0f6d1c17e1a
Jurriaan Bremer <jurriaanbremer@gmail.com>
2013-11-12 02:32:43 +0100

    support for image and routine objects

--- pypin.cpp
+++ pypin.cpp
@@ -13,10 +13,73 @@ namespace Py {
 #define F(name) {#name, &name}
 #define F2(name) {#name, &name##_detour}
 
+static const char *IMG_Name_detour(IMG img);
+static IMG IMG_Open_detour(const char *fname);
+static const char *RTN_Name_detour(RTN rtn);
+static const char *RTN_FindNameByAddress_detour(ADDRINT addr);
+static RTN RTN_CreateAt_detour(ADDRINT addr, const char *name);
 static const char *INS_Mnemonic_detour(INS ins);
 static const char *INS_Disassemble_detour(INS ins);
 
 static void *g_functions[][2] = {
+    // IMG - Image Object
+    F(IMG_Next),
+    F(IMG_Prev),
+    F(IMG_Invalid),
+    F(IMG_Valid),
+    F(IMG_SecHead),
+    F(IMG_SecTail),
+    F(IMG_RegsymHead),
+    F(IMG_Entry),
+    F2(IMG_Name),
+    F(IMG_Gp),
+    F(IMG_LoadOffset),
+    F(IMG_LowAddress),
+    F(IMG_HighAddress),
+    F(IMG_StartAddress),
+    F(IMG_SizeMapped),
+    F(IMG_Type),
+    F(IMG_IsMainExecutable),
+    F(IMG_IsStaticExecutable),
+    F(IMG_Id),
+    F(IMG_FindImgById),
+    F(IMG_FindByAddress),
+    F(IMG_AddInstrumentFunction),
+    F(IMG_AddUnloadFunction),
+    F2(IMG_Open),
+    F(IMG_Close),
+
+    // RTN - Routine Object
+    F(RTN_Sec),
+    F(RTN_Next),
+    F(RTN_Prev),
+    F(RTN_Invalid),
+    F(RTN_Valid),
+    F2(RTN_Name),
+    F(RTN_Sym),
+    F(RTN_Funptr),
+    F(RTN_Id),
+    F(RTN_AddInstrumentFunction),
+    F(RTN_Range),
+    F(RTN_Size),
+    F2(RTN_FindNameByAddress),
+    F(RTN_FindByAddress),
+    F(RTN_FindByName),
+    F(RTN_Open),
+    F(RTN_Close),
+    F(RTN_InsHead),
+    F(RTN_InsHeadOnly),
+    F(RTN_InsTail),
+    F(RTN_NumIns),
+    F(RTN_InsertCall),
+    F(RTN_Address),
+    F2(RTN_CreateAt),
+    F(RTN_Replace),
+
+    // APP
+    F(APP_ImgHead),
+    F(APP_ImgTail),
+
     // INS Instrumentation
     F(INS_AddInstrumentFunction),
     F(INS_InsertCall),
@@ -37,6 +100,50 @@ static void *g_functions[][2] = {
     F(INS_InsertDirectJump),
     F(INS_Delete),
 };
+
+static const char *IMG_Name_detour(IMG img)
+{
+    static char cyclic_strings[32][64]; static uint32_t cyclic_index;
+
+    string s = IMG_Name(img);
+    if(s.c_str() == NULL) return NULL;
+
+    return strcpy(*CYCLIC(cyclic_strings, cyclic_index), s.c_str());
+}
+
+static IMG IMG_Open_detour(const char *fname)
+{
+    // IMG_Open takes a std::string as parameter,
+    // hence the detour function
+    return IMG_Open(fname);
+}
+
+static const char *RTN_Name_detour(RTN rtn)
+{
+    static char cyclic_strings[32][64]; static uint32_t cyclic_index;
+
+    string s = RTN_Name(rtn);
+    if(s.c_str() == NULL) return NULL;
+
+    return strcpy(*CYCLIC(cyclic_strings, cyclic_index), s.c_str());
+}
+
+static const char *RTN_FindNameByAddress_detour(ADDRINT addr)
+{
+    static char cyclic_strings[32][64]; static uint32_t cyclic_index;
+
+    string s = RTN_FindNameByAddress(addr);
+    if(s.c_str() == NULL) return NULL;
+
+    return strcpy(*CYCLIC(cyclic_strings, cyclic_index), s.c_str());
+}
+
+static RTN RTN_CreateAt_detour(ADDRINT addr, const char *name)
+{
+    // RTN_CreateAt takes a std::string as parameter,
+    // hence the detour function
+    return RTN_CreateAt(addr, name);
+}
 
 static const char *INS_Mnemonic_detour(INS ins)
 {
--- pypin.py
+++ pypin.py
@@ -2,6 +2,8 @@ from ctypes import CFUNCTYPE
 from ctypes import c_int, c_char_p, c_void_p
 
 
+IMAGECALLBACK = CFUNCTYPE(None, c_int, c_void_p)
+RTN_INSTRUMENT_CALLBACK = CFUNCTYPE(None, c_int, c_void_p)
 INS_INSTRUMENT_CALLBACK = CFUNCTYPE(None, c_int, c_void_p)
 AFUNPTR = CFUNCTYPE(None)
 
@@ -27,6 +29,64 @@ _gc = []
 # pin function declarations - functions which start with an underscore
 # get special treatment, e.g., they take a callback function
 _pin_function_decl = {
+    # IMG
+    'IMG_Next': _i(c_int),
+    'IMG_Prev': _i(c_int),
+    'IMG_Invalid': _i(),
+    'IMG_Valid': _i(c_int),
+    'IMG_SecHead': _i(c_int),
+    'IMG_SecTail': _i(c_int),
+    'IMG_RegsymHead': _i(c_int),
+    'IMG_Entry': _i(c_int),
+    'IMG_Name': _s(c_int),
+    'IMG_Gp': _i(c_int),
+    'IMG_LoadOffset': _i(c_int),
+    'IMG_LowAddress': _i(c_int),
+    'IMG_HighAddress': _i(c_int),
+    'IMG_StartAddress': _i(c_int),
+    'IMG_SizeMapped': _i(c_int),
+    'IMG_Type': _i(c_int),
+    'IMG_IsMainExecutable': _i(c_int),
+    'IMG_IsStaticExecutable': _i(c_int),
+    'IMG_Id': _i(c_int),
+    'IMG_FindImgById': _i(c_int),
+    'IMG_FindByAddress': _i(c_int),
+    '_IMG_AddInstrumentFunction': _v(IMAGECALLBACK, c_void_p),
+    '_IMG_AddUnloadFunction': _v(IMAGECALLBACK, c_void_p),
+    'IMG_Open': _i(c_char_p),
+    'IMG_Close': _v(c_int),
+
+    # APP
+    'APP_ImgHead': _i(),
+    'APP_ImgTail': _i(),
+
+    # RTN
+    'RTN_Sec': _i(c_int),
+    'RTN_Next': _i(c_int),
+    'RTN_Prev': _i(c_int),
+    'RTN_Invalid': _i(),
+    'RTN_Valid': _i(c_int),
+    'RTN_Name': _s(c_int),
+    'RTN_Sym': _i(c_int),
+    'RTN_Funptr': _i(c_int),
+    'RTN_Id': _i(c_int),
+    '_RTN_AddInstrumentFunction': _v(RTN_INSTRUMENT_CALLBACK, c_void_p),
+    'RTN_Range': _i(c_int),
+    'RTN_Size': _i(c_int),
+    'RTN_FindNameByAddress': _s(c_int),
+    'RTN_FindByAddress': _i(c_int),
+    'RTN_FindByName': _i(c_int, c_char_p),
+    'RTN_Open': _v(c_int),
+    'RTN_Close': _v(c_int),
+    'RTN_InsHead': _i(c_int),
+    'RTN_InsHeadOnly': _i(c_int),
+    'RTN_InsTail': _i(c_int),
+    'RTN_NumIns': _i(c_int),
+    '_RTN_InsertCall': _v(c_int, c_int, AFUNPTR),
+    'RTN_Address': _i(c_int),
+    'RTN_CreateAt': _i(c_int, c_char_p),
+    '_RTN_Replace': CFUNCTYPE(AFUNPTR, c_int, AFUNPTR),
+
     # INS Instrumentation
     '_INS_AddInstrumentFunction': _v(INS_INSTRUMENT_CALLBACK, c_void_p),
     'INS_InsertCall': _v(c_int, c_int, AFUNPTR),
@@ -61,11 +121,46 @@ for name, decl in _pin_function_decl.items():
 
 # the following line(s) are actually not required,
 # but these are for the strict syntax checker(s) - part #2
+_IMG_AddInstrumentFunction = globals()['_IMG_AddInstrumentFunction']
+_IMG_AddUnloadFunction = globals()['_IMG_AddUnloadFunction']
+_RTN_AddInstrumentFunction = globals()['_RTN_AddInstrumentFunction']
+_RTN_InsertCall = globals()['_RTN_InsertCall']
+_RTN_Replace = globals()['_RTN_Replace']
 _INS_AddInstrumentFunction = globals()['_INS_AddInstrumentFunction']
 
 
 # override functions which accept callback functions, because
 # they need some extra care
+def IMG_AddInstrumentFunction(cb, arg):
+    cb = IMAGECALLBACK(cb)
+    _gc.extend((cb, arg))
+    _IMG_AddInstrumentFunction(cb, arg)
+
+
+def IMG_AddUnloadFunction(cb, arg):
+    cb = IMAGECALLBACK(cb)
+    _gc.extend((cb, arg))
+    _IMG_AddUnloadFunction(cb, arg)
+
+
+def RTN_AddInstrumentFunction(cb, arg):
+    cb = RTN_INSTRUMENT_CALLBACK(cb)
+    _gc.extend((cb, arg))
+    _RTN_AddInstrumentFunction(cb, arg)
+
+
+def RTN_InsertCall(rtn, action, cb, *args):
+    cb = AFUNPTR(cb)
+    _gc.append(cb)
+    _RTN_InsertCall(rtn, action, cb, *args)
+
+
+def RTN_Replace(rtn, func):
+    func = AFUNPTR(func)
+    _gc.append(func)
+    _RTN_Replace(rtn, func)
+
+
 def INS_AddInstrumentFunction(cb, arg):
     cb = INS_INSTRUMENT_CALLBACK(cb)
     _gc.extend((cb, arg))
